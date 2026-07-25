238a2b2ebefdba8a1ee940d602baa4be4254f8f8
manpreetkaur0406-bit <mangndec@gmail.com>
2026-07-25 11:27:12 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="AI Resume & Job Market Analytics",
+    page_icon="🤖",
+    layout="wide"
+)
+
+st.title("🤖 AI Resume & Job Market Analytics")
+
+st.markdown("""
+### Welcome!
+
+This project includes:
+
+- 📄 ATS Resume Score
+- 🧠 Skill Gap Analysis
+- 💼 AI Job Recommendation
+- 💰 Salary Prediction
+- 📈 Salary Analytics
+- 🏆 Resume Ranking
+
+Select a page from the sidebar.
+""")
